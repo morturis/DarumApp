edfc7856480acca4497a88e075941dd0fdefc913
RafaelCaballeroMora <rafaelcaballeromora@gmail.com>
2024-04-08 18:17:31 +0200

    connect with backend

--- src/app/external_interfaces/daruma.service.ts
+++ src/app/external_interfaces/daruma.service.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, delay, of } from 'rxjs';
+import { Observable, delay, mergeMap, of } from 'rxjs';
 import {
   DarumaBodyColor,
   DarumaBottomSkin,
   DarumaModel,
   DarumaTopSkin,
 } from '../model/daruma-model';
+import { environment } from 'src/environments/environment';
 
-const BASE_URL = 'http://34.175.64.33:3000';
+const BASE_URL = environment.API_URL;
+
+type BackendDarumaModel = {
+  id: number;
+  objetivo: string;
+  color: number;
+};
 
 @Injectable({
   providedIn: 'root',
@@ -135,72 +142,73 @@ export class DarumaService {
   }
 
   get(id: number): Observable<DarumaModel> {
-    // const headers = {};
-    // return this.http
-    //   .get<DarumaModel>(`${BASE_URL}/daruma/${id}`, {
-    //     headers,
-    //   })
-    //   .pipe(mergeMap((response) => of(this.parseSingleDaruma(response))));
+    const headers = {};
+    return this.http
+      .get<BackendDarumaModel>(`${BASE_URL}/daruma/${id}`, {
+        headers,
+      })
+      .pipe(mergeMap((response) => of(this.parseSingleDaruma(response))));
 
-    const singleDaruma = DarumaService.devValues.find(
-      (daruma) => daruma.id === id,
-    );
-    if (!singleDaruma) throw new Error('404');
-    return of(singleDaruma).pipe(delay(1000));
+    // const singleDaruma = DarumaService.devValues.find(
+    //   (daruma) => daruma.id === id,
+    // );
+    // if (!singleDaruma) throw new Error('404');
+    // return of(singleDaruma).pipe(delay(1000));
   }
 
   getList(userId: number): Observable<DarumaModel[]> {
-    // const headers = {};
-    // return this.http
-    //   .get<DarumaModel[]>(`${BASE_URL}/user/${userId}/daruma`, {
-    //     headers,
-    //   })
-    //   .pipe(mergeMap((response) => of(this.parseDarumaList(response))));
+    const headers = {};
+    //TODO change url when Backend allows filtering by user
+    return this.http
+      .get<BackendDarumaModel[]>(`${BASE_URL}/daruma`, {
+        headers,
+      })
+      .pipe(mergeMap((response) => of(this.parseDarumaList(response))));
 
-    return of(DarumaService.devValues || []).pipe(delay(1000));
+    // return of(DarumaService.devValues || []).pipe(delay(1000));
   }
 
   save(model: DarumaModel): Observable<DarumaModel> {
     if (model.id) return this.update(model);
 
-    // const headers = {};
-    // return this.http
-    //   .post<DarumaModel>(`${BASE_URL}/daruma`, {
-    //     headers,
-    //   })
-    //   .pipe(mergeMap((response) => of(this.parseSingleDaruma(response))));
+    const headers = {};
+    return this.http
+      .post<BackendDarumaModel>(`${BASE_URL}/daruma`, this.parseFrontToBackDaruma(model), {
+        headers,
+      })
+      .pipe(mergeMap((response) => of(this.parseSingleDaruma(response))));
 
-    DarumaService.devValues.push(model);
-    return of(model).pipe(delay(1000));
+    // DarumaService.devValues.push(model);
+    // return of(model).pipe(delay(1000));
   }
   update(model: DarumaModel): Observable<DarumaModel> {
-    // const headers = {};
-    // return this.http
-    //   .put<DarumaModel>(`${BASE_URL}/daruma/${model.id}`, {
-    //     headers,
-    //   })
-    //   .pipe(mergeMap((response) => of(this.parseSingleDaruma(response))));
+    const headers = {};
+    return this.http
+      .put<BackendDarumaModel>(`${BASE_URL}/daruma/${model.id}`, this.parseFrontToBackDaruma(model), {
+        headers,
+      })
+      .pipe(mergeMap((response) => of(this.parseSingleDaruma(response))));
 
-    const index = DarumaService.devValues.findIndex(
-      (daruma) => daruma.id === model.id,
-    );
-    if (!index) throw new Error('404');
-    DarumaService.devValues[index] = model;
-    return of(model).pipe(delay(1000));
+    // const index = DarumaService.devValues.findIndex(
+    //   (daruma) => daruma.id === model.id,
+    // );
+    // if (!index) throw new Error('404');
+    // DarumaService.devValues[index] = model;
+    // return of(model).pipe(delay(1000));
   }
 
   delete(model: DarumaModel): Observable<DarumaModel> {
-    // const headers = {};
-    // return this.http.delete<DarumaModel>(`${BASE_URL}/daruma/${model.id}`, {
-    //   headers,
-    // })
-    //   .pipe(mergeMap((response) => of(this.parseSingleDaruma(response))));
+    const headers = {};
+    return this.http.delete<BackendDarumaModel>(`${BASE_URL}/daruma/${model.id}`, {
+      headers,
+    })
+      .pipe(mergeMap((response) => of(this.parseSingleDaruma(response))));
 
     //Keeps all except for the deleted element
-    DarumaService.devValues = DarumaService.devValues.filter(
-      (daruma) => daruma.id !== model.id,
-    );
-    return of(model).pipe(delay(1000));
+    // DarumaService.devValues = DarumaService.devValues.filter(
+    //   (daruma) => daruma.id !== model.id,
+    // );
+    // return of(model).pipe(delay(1000));
   }
 
   /**
@@ -208,8 +216,25 @@ export class DarumaService {
    * @param response
    * @returns
    */
-  private parseSingleDaruma(response: DarumaModel): DarumaModel {
-    return response;
+  private parseSingleDaruma(response: BackendDarumaModel): DarumaModel {
+
+    const colorMappings: Record<number, DarumaBodyColor> = {
+      [1]: DarumaBodyColor.BLUE,
+      [2]: DarumaBodyColor.RED,
+      [3]: DarumaBodyColor.YELLOW,
+      [4]: DarumaBodyColor.PINK,
+    };
+
+    const parsedDaruma: DarumaModel = {
+      leftEye: false,
+      rightEye: false,
+      bodyColor: colorMappings[response.color],
+      goals: response.objetivo,
+      topSkin: DarumaTopSkin.AUREOLA,
+      bottomSkin: DarumaBottomSkin.BIGOTE,
+      id: response.id,
+    };
+    return parsedDaruma;
   }
 
   /**
@@ -217,7 +242,23 @@ export class DarumaService {
    * @param response
    * @returns
    */
-  private parseDarumaList(response: DarumaModel[]): DarumaModel[] {
+  private parseDarumaList(response: BackendDarumaModel[]): DarumaModel[] {
     return response.map(this.parseSingleDaruma);
+  }
+  private parseFrontToBackDaruma(model: DarumaModel): BackendDarumaModel {
+    const colorMappings: Record<DarumaBodyColor, number> = {
+      [DarumaBodyColor.BLUE]: 1,
+      [DarumaBodyColor.RED]: 2,
+      [DarumaBodyColor.YELLOW]: 3,
+      [DarumaBodyColor.PINK]: 4,
+      [DarumaBodyColor.EMPTY_DOTTED]: 0,
+    };
+
+    const backendDaruma: BackendDarumaModel = {
+      id: model.id || -1, //This should be ignored by the backend
+      color: colorMappings[model.bodyColor],
+      objetivo: model.goals
+    };
+    return backendDaruma;
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  API_URL: "http://34.175.176.53:8080",
 };
